82ecaac459cb4685caf43df3562689bbc646cb4e
J Grant Tuttle <Grant.tuttle@gmail.com>
2022-09-30 15:09:08 -0400

    Add create user api for hook from Auth0

---
+++ pages/api/user/index.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client';
+
+interface newUserBody {
+  email: string,
+  name: string
+}
+
+const handleNewUser = async (email: string) => {
+  const prisma = new PrismaClient();
+  const createMemberResponse = await prisma.member.create({
+    data: {
+      email,
+      boards: {
+        create: [
+          { 
+            board: { 
+              connect: { 
+                id: 1 
+              } 
+            } 
+          },
+        ],
+      }
+    }
+  });
+  return createMemberResponse
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { email } = req.body as newUserBody;
+    if (email) {
+      const newUser = await handleNewUser(email);
+      res.status(200).send({ data: newUser });
+    } else {
+      return res.status(500).send({ error: "No email found on the create user request..." })
+    }
+  }
+}
